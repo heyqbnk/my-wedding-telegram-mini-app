f4ceaf20ca6713c97c8b3e3098eac5c3423f2587
Vladislav Kibenko <34907325+heyqbnk@users.noreply.github.com>
2026-03-06 22:47:49 +0500

    chore: temp disable HTTPS

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,20 @@
-import { fileURLToPath, URL } from 'node:url'
+import vue from '@vitejs/plugin-vue';
+import { fileURLToPath, URL } from 'node:url';
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
-import mkcert from 'vite-plugin-mkcert'
+import { defineConfig } from 'vite';
+// import mkcert from 'vite-plugin-mkcert';
+import vueDevTools from 'vite-plugin-vue-devtools';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     vueDevTools(),
-    mkcert(),
+    // mkcert(),
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
-})
+});
