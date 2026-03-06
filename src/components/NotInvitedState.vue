251eca9c9a577fa4b36bf59add991d15eea80c5f
Vladislav Kibenko <34907325+heyqbnk@users.noreply.github.com>
2026-03-06 22:53:11 +0500

    feat(components): add not invited stub

---
+++ src/components/NotInvitedState.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div class="not-invited-state">
+    Упс! Кажется, Вас не пригласили
+  </div>
+</template>
+
+<style lang="scss">
+.not-invited-state {
+  position: fixed;
+  inset: 0;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+</style>
